9ff5654d4f0b9bca1652af39c06c506c7424fd03
Samuel De Backer <sdebacker@gmail.com>
2026-05-01 13:23:36 +0200

    blade files formatted

--- resources/views/admin/sidebar/group.blade.php
+++ resources/views/admin/sidebar/group.blade.php
@@ -1,8 +1,6 @@
 <li class="sidebar-panel sidebar-panel-{{ $group->id }}">
     @if ($group->shouldShowHeading())
-        <a class="sidebar-panel-title @if (config('typicms.user.menus_' . $group->id . '_collapsed')) collapsed @endif" href="#{{ $group->id }}" data-bs-toggle="collapse">
-            {{ $group->name }}
-        </a>
+        <a class="sidebar-panel-title @if (config('typicms.user.menus_' . $group->id . '_collapsed')) collapsed @endif" href="#{{ $group->id }}" data-bs-toggle="collapse">{{ $group->name }}</a>
     @endif
     <ul class="sidebar-panel-collapse collapse @if (!config('typicms.user.menus_' . $group->id . '_collapsed')) show @endif" id="{{ $group->id }}">
         @foreach ($group->getItems() as $item)
--- resources/views/admin/sidebar/item.blade.php
+++ resources/views/admin/sidebar/item.blade.php
@@ -1,8 +1,6 @@
 <li class="sidebar-item sidebar-item-{{ $item->id }} {{ $item->state }}">
     <a class="sidebar-item-link" href="{{ $item->route }}">
-        <span class="sidebar-item-link-icon icon">
-            {!! $item->icon ?? '<i class="icon-grid-2x2"></i>' !!}
-        </span>
+        <span class="sidebar-item-link-icon icon">{!! $item->icon ?? '<i class="icon-grid-2x2"></i>' !!}</span>
         <span class="sidebar-item-link-label">{{ $item->name }}</span>
     </a>
 </li>
